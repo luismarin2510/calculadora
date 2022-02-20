6d76c603399e6e9181cc04050dc6c9ca2d6b0a41
luismarin2510 <opraedu@.com>
2022-02-20 14:23:18 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let screen = document.querySelector("#screen-result");
+
+function getData(ref) {
+    let value = ref.value;
+    screen.value +=value;
+    Math.sqrt();
+}
+
+function clean(){
+    screen.value = '';
+}
+
+function calculate(){
+    try {
+        screen.value = eval(screen.value);
+    } 
+    catch (error) {
+        screen.value = 'Error';
+        setTimeout (() => {
+            clean();
+        }, 900);
+    }
+}
