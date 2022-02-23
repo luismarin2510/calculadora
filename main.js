09a4c1f7d0a20ebbebeab56dd29c0778655f541c
luismarin2510 <opraedu@.com>
2022-02-22 20:34:30 -0500

    funciones con java de raiz y %

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ let screen = document.querySelector("#screen-result");
 function getData(ref) {
     let value = ref.value;
     screen.value +=value;
-    Math.sqrt();
 }
-
+function raiz() {
+    screen.value = Math.sqrt(screen.value);
+}
+function  porcentaje() {
+    screen.value = screen.value/=100;
+}
 function clean(){
     screen.value = '';
 }
