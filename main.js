60160f924fb697c7ff5935b73151a34c129ddf2e
luismarin2510 <opraedu@.com>
2022-02-24 17:18:28 -0500

    funcion DEL

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 let screen = document.querySelector("#screen-result");
-
+/*  */
 function getData(ref) {
     let value = ref.value;
     screen.value +=value;
 }
+/* funcion de raiz */
 function raiz() {
     screen.value = Math.sqrt(screen.value);
 }
 function  porcentaje() {
     screen.value = screen.value/=100;
 }
+/* limpiar pantalla  */
 function clean(){
     screen.value = '';
+}
+/* funcion de retroceso  */
+function DEL() {
+    screen.value = new String(screen.value).substring(0,
+        new String(screen.value).length - 1);
 }
 
 function calculate(){
